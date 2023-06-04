d3ea51ee6a3f2bf590ca7646cf691c37709517a6
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-06-04 17:50:09 +0530

    Correcting Nginx DNS config entries

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,11 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
 
-systemctl enable mongod
-systemctl start mongod
+
 
 ## Edit the file and replace 127.0.0.1 to 0.0.0.0
+sed -e 's|127.0.0.1|0.0.0.0|' /etc/mongodb.conf
 
+systemctl enable mongod
+systemctl start mongod
 systemctl restart mongod
