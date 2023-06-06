164d31a746032284de10f2a5bbffb03e1362a09e
nagula-anjaneyulu <anjaneyulu.grepthor@gmail.com>
2023-06-07 00:13:45 +0530

    echo command

--- redis.sh
+++ redis.sh
@@ -9,7 +9,7 @@ echo -e "\e[36m>>>>>>>>>>Update listen address<<<<<<<\e[0m"
 sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 
-echo -e "\e[36m>>>>>>>>>>start redis service<<<<<<<\e[0m
+echo -e "\e[36m>>>>>>>>>>start redis service<<<<<<<\e[0m"
 systemctl enable redis
 systemctl restart redis
 
